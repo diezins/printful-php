ce453d4ed66d81e2ac18615d16d70ba2408fbcdf
daaaaaavis <daaaaaavis@gmail.com>
2018-08-01 01:23:19 +0300

    added data insertion to db when starting new survey

--- start.php
+++ start.php
@@ -2,15 +2,32 @@
     // after pressing start
     session_start();
 
-    // session_destroy();   
+    require_once '/mySQL/database.php';
+
+    $db = getConnection();
+
+    //session_destroy();   
     if (isset($_POST['username']) && trim($_POST['username']) != '' && isset($_POST['testID'])) {
         $_SESSION['username'] = $_POST['username'];
         $_SESSION['testID'] = $_POST['testID'];
-    }
 
-    if (isset($_SESSION['username']) && isset($_SESSION['testID'])) {
-        $url = "survey";
-        header('Location: ' . $url);
-        exit();
+        if (isset($_SESSION['username']) && isset($_SESSION['testID'])) {
+            $input_username =$_SESSION['username'];
+            $survey_id = $_SESSION['testID'];
+    
+            $sql_query = "INSERT INTO user (username, survey_id) VALUES (? , ?)";
+            
+            $stmt = $db->prepare($sql_query);
+            $stmt->bind_param('sd', $input_username, $survey_id);
+            $is_successful = $stmt->execute();
+            if($is_successful){
+                // Records created successfully. Redirect to landing page
+                    $url = "survey";
+                    header('Location: ' . $url);
+                    exit();
+            } else{
+                echo "Something went wrong. Please try again later.";
+            }
+        }
     }
 ?>
